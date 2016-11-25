8ce6875387488747f6d409828ab5d728f9e66857
bjcrane <bjcrane2013@gmail.com>
2016-11-25 14:45:34 +0000

    Updating SQL Files
    Little tweaks. Kind of important

--- SQL/Stored_Procedures.sql
+++ SQL/Stored_Procedures.sql
@@ -1,3 +1,4 @@
+DROP PROCEDURE IF EXISTS sp_surveys_about_professor;
 
 DELIMITER $$
 CREATE PROCEDURE sp_surveys_about_professor(IN $professorID int)
@@ -48,3 +49,54 @@ DELIMITER ;
 CALL sp_professors_for_a_course('CSC 3326');
 
 -- ------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_answer_question_choice;
+
+DELIMITER $$
+CREATE PROCEDURE sp_answer_question_choice(IN $surveyID int, IN $questionID int, IN $offeredAnswerID int)
+BEGIN
+    
+    INSERT INTO Answer_Choice VALUES ($surveyID, $questionID, $offeredAnswerID);
+
+END;
+$$
+DELIMITER ;
+
+CALL sp_answer_question_choice();
+
+-- ------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_answer_question_text;
+
+DELIMITER $$
+CREATE PROCEDURE sp_answer_question_text(IN $surveyID int, IN $questionID int, IN $textAnswer varchar(500))
+BEGIN
+    
+    INSERT INTO Answer_Text (surveyID, questionId, answer) 
+    VALUES ($surveyID, $questionID, $textAnswer);
+
+END;
+$$
+DELIMITER ;
+
+CALL sp_answer_question_text();
+
+-- ------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_answer_question_text;
+
+DELIMITER $$
+CREATE PROCEDURE sp_display_question_with_choices(IN $surveyID int, IN $questionID int, IN $textAnswer varchar(500))
+BEGIN
+    
+    INSERT INTO Answer_Text (surveyID, questionId, answer) 
+    VALUES ($surveyID, $questionID, $textAnswer);
+
+END;
+$$
+DELIMITER ;
+
+CALL sp_answer_question_text();
+
+-- ------------------------------------------------------------------
+
