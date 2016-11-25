c316b57ec7a00ac9e7a2eb2070a5c4d65bf6542b
bjcrane <bjcrane2013@gmail.com>
2016-11-25 15:36:32 +0000

    Adding stored procedures
    related to taking survey and viewing results

--- SQL/Stored_Procedures.sql
+++ SQL/Stored_Procedures.sql
@@ -86,17 +86,70 @@ CALL sp_answer_question_text();
 DROP PROCEDURE IF EXISTS sp_answer_question_text;
 
 DELIMITER $$
-CREATE PROCEDURE sp_display_question_with_choices(IN $surveyID int, IN $questionID int, IN $textAnswer varchar(500))
+CREATE PROCEDURE sp_display_question_answer_choices(IN $questionID int)
 BEGIN
     
-    INSERT INTO Answer_Text (surveyID, questionId, answer) 
-    VALUES ($surveyID, $questionID, $textAnswer);
+    SELECT offeredAnswerID, answerText 
+    FROM Question_Answer NATURAL JOIN OfferedAnswer
+    WHERE questionID = $questionID;
 
 END;
 $$
 DELIMITER ;
 
-CALL sp_answer_question_text();
+CALL sp_display_question_answer_choices(1);
 
 -- ------------------------------------------------------------------
 
+DROP PROCEDURE IF EXISTS sp_display_question_answer_statistics;
+
+DELIMITER $$
+CREATE PROCEDURE sp_display_question_answer_statistics(IN $questionID int)
+BEGIN
+    
+    SELECT offeredAnswerID, answerText, percent
+    FROM Question_Answer 
+		NATURAL JOIN OfferedAnswer
+		NATURAL JOIN Question_Answer_Statistics
+    WHERE questionID = $questionID;
+
+END;
+$$
+DELIMITER ;
+
+CALL sp_display_question_answer_choices(1);
+
+-- ------------------------------------------------------------------
+
+SELECT offeredAnswerID, answerText, percent
+    FROM Question_Answer 
+		NATURAL JOIN OfferedAnswer
+		NATURAL JOIN Question_Answer_Statistics
+    WHERE questionID = 1;
+    
+    
+-- ------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_display_question_answer_statistics;
+
+DELIMITER $$
+CREATE PROCEDURE sp_display_answer_text_for_question(IN $questionID int)
+BEGIN
+    
+    SELECT answer, voteCount
+    FROM Question
+		NATURAL JOIN Answer_Text
+    WHERE questionID = $questionID;
+
+END;
+$$
+DELIMITER ;
+
+CALL sp_display_question_answer_choices(1);
+
+SELECT answer, voteCount
+    FROM Question
+		NATURAL JOIN Answer_Text
+    WHERE questionID = 2;
+
+-- ------------------------------------------------------------------
