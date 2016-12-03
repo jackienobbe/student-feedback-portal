90260aa07f807f90bfe018b788c8c6218c8565a0
Jackie Nobbe <jac.nobbe@gmail.com>
2016-12-03 00:36:31 +0000

    Work on review query
    doesn’t currently werk :/

--- includes/disp_reviews.inc.php
+++ includes/disp_reviews.inc.php
@@ -16,13 +16,27 @@ class ReviewModel
     return $this->db->query('SELECT questionID, questionText FROM Question WHERE answerTypeID = 2');
   }
 
-  public function getAllReviews() {
-    return $this->db->query('SELECT questionID, offeredAnswerID, answerText
-      FROM OfferedAnswer NATURAL JOIN Question_Answer
-      ORDER BY questionID ');
+  public function getReviewForQuestion( int $inQuestionID ) {
+    $qID = $inQuestionID;
+    // return $this->db->query('SELECT questionID, offeredAnswerID, answerText
+    //  FROM OfferedAnswer NATURAL JOIN Question_Answer WHERE questionID = $qID
+    //  ORDER BY questionID ASC, offeredAnswerID ASC '); // ->bindParam(':inQuestionID', $qID);
+    $sql = ('SELECT questionID, offeredAnswerID, answerText
+      FROM OfferedAnswer NATURAL JOIN Question_Answer WHERE questionID = :qID
+      ORDER BY questionID ASC, offeredAnswerID ASC');
+
+      $sth = $this->db->prepare($sql);
+      $sth->bindParam(':qID', $qID);
+      return $sth->execute();
+      
     }
   }
 
+  // public function getAllReview(){
+  //   return $this->db->query('SELECT questionID, offeredAnswerID, answerText
+  //     FROM OfferedAnswer NATURAL JOIN Question_Answer
+  //     ORDER BY questionID ASC, offeredAnswerID asc ');
+  //   }
 
   // Connect to database server
   include 'db_connect.php';
@@ -30,64 +44,37 @@ class ReviewModel
   $questionModel = new ReviewModel($dbh);
   $questionList = $questionModel->getAllQuestions();
 
-  $reviewModel = new ReviewModel($dbh);
-  $reviewList = $reviewModel->getAllReviews();
+  // $reviewModel = new ReviewModel($dbh);
+  // $reviewList = $reviewModel->getAllReviews();
 
   try {
+
     $courseID = $_POST['courseID'];
     $professorID = $_POST['professorID'];
 
-    // $statement = $dbh->query("SELECT questionID FROM Question_Answer_Statistics_By_Section");
-    // $statement->bindParam(':courseID', $courseID);
-    // $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-    // foreach( $reviewList as $reviewRow ){
-    //   if( $reviewRow['questionID'] == $questionRow['questionID'] ){
-    //     echo "<li> " . $reviewRow['answerText'] . " " . $reviewRow['questionID'] . " </li> \n";
-    //   }
-    // }
     echo "<ul>\n";
-    $reviewRowCount = $reviewList->rowCount();
-    $questionID = '';
+    //$reviewRowCount = $reviewList->rowCount();
+    //$questionID = '';
     // $reviewList as $reviewRow;
 
     foreach( $questionList as $questionRow ) {
-      echo "<li> " . $questionRow['questionText']." </li>\n";
+
+      echo "<li> " . $questionRow['questionText'] ." </li>\n";
       echo "<ul>\n";
 
-      //$questionID = $questionRow['questionID'];
+      $reviewModel = new ReviewModel($dbh);
+      $reviewList = $reviewModel->getReviewForQuestion( $questionRow['questionID'] );
+      echo $reviewList;
 
       foreach( $reviewList as $reviewRow ){
-        // for( $i = 0; $i < $reviewRowCount; $i++ ){
-        //echo "yoho!";
-
-        if( $reviewRow['questionID'] == $questionRow['questionID'] ){
-          echo "<li> " . $reviewRow['answerText'] . " </li> \n";
-        }
-        else break;
+        echo "<li> " . $reviewRow['answerText'] . " </li> \n";
       }
-      // }
       echo "</ul>\n";
+      unset($reviewModel);
+      unset($reviewRow);
     }
     echo "</ul>\n";
 
-    // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // $sql = "SELECT question, courseName FROM Course WHERE courseID = :courseID;";
-    // $sth = $dbh->prepare($sql);
-    // $sth->bindParam(':courseID', $courseID);
-    // $sth->execute();
-    // $result = $sth->setFetchMode(PDO::FETCH_ASSOC);
-
-    //   if(count($result) > 1)
-    //   {
-    //     echo "<table>\n";
-    //
-    //     // set the resulting array to associative
-    //     foreach(new ListItems(new RecursiveArrayIterator($sth->fetchAll())) as $k=>$v) {
-    //       echo $v . " ";
-    //     }
-    //     echo "</table>\n";
-    //   }
     //   else {
     //     echo "<p>There aren't any reviews for this course by this professor yet...</p>\n";
     //     if(isset($_SESSION['userID']))
