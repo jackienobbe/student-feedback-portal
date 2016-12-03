2ae80f7882107d2c26e0616b7c354e844026f121
Jackie Nobbe <jac.nobbe@gmail.com>
2016-12-03 19:11:45 +0000

    WORKING DISPLAY OF ANSWERRRZZZZZZ
    WOIWOWOOWOWOWOWOW THIS IS WHY IM A CS MAJER

--- includes/disp_reviews.inc.php
+++ includes/disp_reviews.inc.php
@@ -17,75 +17,77 @@ class ReviewModel
   }
 
   public function getReviewForQuestion( int $inQuestionID ) {
-    $qID = $inQuestionID;
+    // $qID = $inQuestionID;
     // return $this->db->query('SELECT questionID, offeredAnswerID, answerText
     //  FROM OfferedAnswer NATURAL JOIN Question_Answer WHERE questionID = $qID
     //  ORDER BY questionID ASC, offeredAnswerID ASC '); // ->bindParam(':inQuestionID', $qID);
-    $sql = ('SELECT questionID, offeredAnswerID, answerText
-      FROM OfferedAnswer NATURAL JOIN Question_Answer WHERE questionID = :qID
-      ORDER BY questionID ASC, offeredAnswerID ASC');
-
-      $sth = $this->db->prepare($sql);
-      $sth->bindParam(':qID', $qID);
+    // $sql = ('SELECT questionID, offeredAnswerID, answerText
+    //   FROM OfferedAnswer NATURAL JOIN Question_Answer WHERE questionID = :qID
+    //   ORDER BY questionID ASC, offeredAnswerID ASC');
+    //
+    //   $sth = $this->db->prepare($sql);
+    //   $sth->bindParam(':qID', $qID);
       return $sth->execute();
-      
+
     }
-  }
 
-  // public function getAllReview(){
-  //   return $this->db->query('SELECT questionID, offeredAnswerID, answerText
-  //     FROM OfferedAnswer NATURAL JOIN Question_Answer
-  //     ORDER BY questionID ASC, offeredAnswerID asc ');
-  //   }
+    public function getAllReviews() {
+      return $this->db->query('SELECT questionID, offeredAnswerID, answerText
+        FROM OfferedAnswer NATURAL JOIN Question_Answer
+       ORDER BY questionID ASC, offeredAnswerID ASC ');
+      }
+    }
+    // Connect to database server
+    include 'db_connect.php';
 
-  // Connect to database server
-  include 'db_connect.php';
+    $questionModel = new ReviewModel($dbh);
+    $questionList = $questionModel->getAllQuestions();
 
-  $questionModel = new ReviewModel($dbh);
-  $questionList = $questionModel->getAllQuestions();
+    // $reviewModel = new ReviewModel($dbh);
+    // $reviewList = $reviewModel->getAllReviews();
 
-  // $reviewModel = new ReviewModel($dbh);
-  // $reviewList = $reviewModel->getAllReviews();
+    try {
 
-  try {
+      $courseID = $_POST['courseID'];
+      $professorID = $_POST['professorID'];
 
-    $courseID = $_POST['courseID'];
-    $professorID = $_POST['professorID'];
-
-    echo "<ul>\n";
-    //$reviewRowCount = $reviewList->rowCount();
-    //$questionID = '';
-    // $reviewList as $reviewRow;
-
-    foreach( $questionList as $questionRow ) {
-
-      echo "<li> " . $questionRow['questionText'] ." </li>\n";
       echo "<ul>\n";
+      //$reviewRowCount = $reviewList->rowCount();
+      //$questionID = '';
+      // $reviewList as $reviewRow;
 
-      $reviewModel = new ReviewModel($dbh);
-      $reviewList = $reviewModel->getReviewForQuestion( $questionRow['questionID'] );
-      echo $reviewList;
+      foreach( $questionList as $questionRow ) {
 
-      foreach( $reviewList as $reviewRow ){
-        echo "<li> " . $reviewRow['answerText'] . " </li> \n";
+        echo "<li> " . $questionRow['questionText'] ." </li>\n";
+        echo "<ul>\n";
+
+        $reviewModel = new ReviewModel($dbh);
+        $reviewList = $reviewModel->getAllReviews();
+
+        foreach( $reviewList as $reviewRow ){
+          // echo "<li> " . $reviewRow['answerText'] . " </li> \n";
+          if($reviewRow['questionID'] == $questionRow['questionID'])
+          {
+            echo "<li> " . $reviewRow['answerText'] . " </li> \n";
+          }
+        }
+        echo "</ul>\n";
+        unset($reviewModel);
+        unset($reviewRow);
       }
       echo "</ul>\n";
-      unset($reviewModel);
-      unset($reviewRow);
-    }
-    echo "</ul>\n";
 
-    //   else {
-    //     echo "<p>There aren't any reviews for this course by this professor yet...</p>\n";
-    //     if(isset($_SESSION['userID']))
-    //     {
-    //       echo "<p>Taking this course this semester? <a href='#'> Enroll and add a review!</a></p>\n";
-    //     }
-    //   }
-    $dbh = null;
-    //
-  }
-  catch(PDOException $e) {
-    $dbh = null;
-    header("Location: error.php?err=" . $e->getMessage());
-  }
+      //   else {
+      //     echo "<p>There aren't any reviews for this course by this professor yet...</p>\n";
+      //     if(isset($_SESSION['userID']))
+      //     {
+      //       echo "<p>Taking this course this semester? <a href='#'> Enroll and add a review!</a></p>\n";
+      //     }
+      //   }
+      $dbh = null;
+      //
+    }
+    catch(PDOException $e) {
+      $dbh = null;
+      header("Location: error.php?err=" . $e->getMessage());
+    }
