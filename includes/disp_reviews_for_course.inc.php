ebe064754183800ec6dc18afb0d470fbd959a95a
bjcrane <bjcrane2013@gmail.com>
2016-12-05 12:31:21 +0000

    View course review almost working
    Minor changes to SQL. More changes to SQL soon

--- includes/disp_reviews_for_course.inc.php
+++ includes/disp_reviews_for_course.inc.php
@@ -3,6 +3,7 @@
 // disp_reviews.inc.php
 //
 
+
 class ReviewModel
 {
   protected $dbh;
@@ -20,11 +21,45 @@ class ReviewModel
     return $this->db->query('SELECT questionID, questionText FROM Question WHERE answerTypeID = 1');
   }
 
-  public function getPossibleAnswers() {
-    return $this->db->query('SELECT questionID, offeredAnswerID, answerText
-      FROM OfferedAnswer NATURAL JOIN Question_Answer
-      ORDER BY questionID ASC, offeredAnswerID ASC ');
+  // public function getPossibleAnswers($courseID, $professorID) {
+  //   $sth = $this->db->prepare('SELECT questionID, offeredAnswerID, answerText, percent
+  //          FROM Question_Answer
+  // 	       NATURAL JOIN OfferedAnswer NATURAL JOIN  Question_Answer_Statistics_By_Course_And_Professor
+  //            WHERE courseID = :courseID
+  //            AND professorID = :professorID
+  //            GROUP BY questionID ASC, offeredAnswerID ASC');
+  //
+  //   $sth->bindParam(':courseID', $courseID);
+  //   $sth->bindParam(':professorID', $professorID);
+  //
+  //   return $sth->execute();
+  //
+  // }
+
+  public function getAllReviews() {
+    $sth = $this->db->query('SELECT questionID, offeredAnswerID, answerText, percent
+           FROM Question_Answer
+  	       NATURAL JOIN OfferedAnswer NATURAL JOIN  Question_Answer_Statistics_By_Course_And_Professor
+             GROUP BY questionID ASC, offeredAnswerID ASC');
+
+    return $sth;
+
     }
+
+  // public function getPossibleAnswers() {
+  //   $stmt = $dbh->prepare("SELECT questionID, offeredAnswerID, answerText, percent
+  //       FROM Question_Answer
+	//       NATURAL JOIN OfferedAnswer NATURAL JOIN  Question_Answer_Statistics_By_Course_And_Professor
+  //         WHERE courseID = ?
+  //         GROUP BY questionID ASC, offeredAnswerID ASC");
+  //
+  //     if ($stmt->execute(array($_POST['courseID']))) {
+  //       while ($row = $stmt->fetch()) {
+  //         print_r($row);
+  //       }
+  //     }
+  //   }
+
   }
   // Connect to database server
   include 'db_connect.php';
@@ -43,28 +78,30 @@ class ReviewModel
     $courseID = $_POST['courseID'];
     $professorID = $_POST['professorID'];
 
-    echo "<ul>\n";
+    echo "<ul class='reviewList' >\n";
     //$reviewRowCount = $reviewList->rowCount();
     //$questionID = '';
     // $reviewList as $reviewRow;
 
     foreach( $choiceQuestionList as $choiceQuestionRow ) {
 
-      echo "<li> " . $choiceQuestionRow['questionText'] ." </li>\n";
+      echo "<li class='reviewList'> " . $choiceQuestionRow['questionText'] ." </li>\n";
       // echo "<ul>\n";
 
       $answerModel = new ReviewModel($dbh);
-      $answerList = $answerModel->getPossibleAnswers();
+      $answerList = $answerModel->getAllReviews();
 
+      // TODO: TEST FOR COURSE AND PROFESOR TOO
       echo "<table>";
       foreach( $answerList as $answerRow ){
         if($answerRow['questionID'] == $choiceQuestionRow['questionID'])
         {
           echo "<tr> <td>" . $answerRow['answerText'] . " </td><td> " .
-          $answerRow['statistics'] . "</td></tr> \n";
+          $answerRow['percent'] . "</td></tr> \n";
         }
       }
       echo "</table>";
+      echo "</br>";
     }
     foreach( $textQuestionList as $textQuestionRow) {
       echo "<li> " . $textQuestionRow['questionText'] . "</li>"
